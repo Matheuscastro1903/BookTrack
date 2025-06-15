9a829b8147b6cb7eaa4584945b845a408919bc29
Matheus de Castro Pecora <matheusdecastro1903@gmail.com>
2025-06-15 01:14:08 -0300

    attprojeto
    atualização de algumas partes do projeto

--- BookTrack.py
+++ BookTrack.py
@@ -1,6 +1,9 @@
 import customtkinter as ctk
+import tkinter as tk
+from tkinter import ttk
 import requests
 import json
+import re
 
 # Carregar o dicionário de estados e siglas a partir do arquivo JSON
 with open("estados.json", "r", encoding="utf-8") as arquivo:
@@ -22,17 +25,51 @@ def voltar_inicial():
     frame_topoinicial.pack(fill="x")
     tela_inicial.pack(fill="both",expand=True)
 
-
-
-def validar_numeros(novo_texto):  # Adicione o parâmetro
-    return novo_texto.isdigit() or novo_texto == ""
-
-def validar_letras_espacos(novo_texto):  # Adicione o parâmetro
-    return all(c.isalpha() or c.isspace() for c in novo_texto) or novo_texto == ""
-
 def cadastrar_conta():
-    #passar os valores chamando a classe cadastro para ser verificado
+    nome = entrada_nome.get()
+    idade = entrada_idade.get()
+    email = entrada_email.get()
+    senha = entrada_senha.get()
+    livros_fisicos = entrada_livrosfisicos.get()
+    livros_digitais = entrada_livrosdigitais.get()
+    horas_estudo = entrada_horas.get()
+    horas_entretenimento = entrada_entretenimento.get()
+    preferencia = combobox_preferencia.get()  
+    estado = combobox_estado.get()
+    cidade = combobox_cidade.get()
+
+    entradas = [
+        nome, idade, email, senha,
+        livros_fisicos, livros_digitais,
+        horas_estudo, horas_entretenimento
+    ]
+    #parar qualquer campo vazio na lista entradas,irá retornar o label aviso="Campos não preenchidos ainda"
+    if any(campo == "" for campo in entradas):
+        label_aviso.configure(text="Campos não preenchidos ainda.", text_color="red")
+        #Return usado para parar a função
+        return
+    #Aqui terá a verificação de cada combobox em relação aos "valores iniciais"
+    if preferencia == "Selecione sua preferência de leitura":
+        label_aviso.configure(text="Selecione sua preferência de leitura.", text_color="red")
+        return
+    if estado == "Selecione o estado":
+        label_aviso.configure(text="Selecione o estado.", text_color="red")
+        return
+    if cidade == "Primeiro selecione o estado" or not cidade:
+        label_aviso.configure(text="Selecione a cidade.", text_color="red")
+        return
+    
+
+    cadastro = Cadastro(nome, idade, email, senha,livros_fisicos, livros_digitais,horas_estudo, horas_entretenimento,preferencia, estado, cidade)
+
+
+
+
+# Define a função principal de cadastro de conta
+def cadastrar_conta():
     pass
+
+
 
 def voltar_menu():
 
@@ -46,7 +83,7 @@ def obter_cidades(sigla_estado):
         dados = resposta.json()
         return [cidade["nome"] for cidade in dados]
     except:
-        return ["Erro ao carregar cidades"]
+        label_aviso.configure(text="Erro ao carregar cidades. Verifique sua conexão.",text_color="Red")
 
 
 # Função chamada quando um estado é selecionado
@@ -55,9 +92,18 @@ def atualizar_cidades(event=None):
     if sigla:
         combobox_cidade.set("Carregando...")
         janela.update()
+        #dentro da variável cidade ficará armazada toda a lista de cidades procuradas com a API
         cidades = obter_cidades(sigla)
+        #atualização dos comboboxcidade
         combobox_cidade.configure(values=cidades)
+        #
         combobox_cidade.set("Selecione a cidade")
+
+def validar_numeros(novo_texto):  # Adicione o parâmetro
+    return novo_texto.isdigit() or novo_texto == ""
+
+def validar_letras_espacos(novo_texto):  # Adicione o parâmetro
+    return all(c.isalpha() or c.isspace() for c in novo_texto) or novo_texto == ""
 
 
 
@@ -65,7 +111,7 @@ ctk.set_appearance_mode("light")  # ou "light"
 
 janela = ctk.CTk()
 janela.title("BookTrack")
-janela.geometry("500x600+400+150")
+janela.geometry("650x750+400+150")
 janela.resizable(False, False)
 
 ################################################################
@@ -146,71 +192,93 @@ frame_login=ctk.CTkFrame(janela,fg_color="#ffffff")
 #frame cadastro
 frame_cadastro=ctk.CTkFrame(janela,fg_color="#ffffff")
 label_cadastro=ctk.CTkLabel(frame_cadastro,text="Informe seus dados:",fg_color="#ffffff",text_color="blue",font=("Arial", 20))
-label_cadastro.pack(pady=10)
+label_cadastro.pack(pady=2)
 label_aviso=ctk.CTkLabel(frame_cadastro,text=" ",fg_color="#ffffff",text_color="blue",font=("Arial", 20))
-label_aviso.pack(pady=10)
+label_aviso.pack(pady=2)
 
 #entrada de dados
 #widthe serve para aumentar o tamanho da caixa de entrada horizontalmente
-#
+
+#1-Entrada Nome
 label_nome = ctk.CTkLabel(frame_cadastro, text="Nome Completo (apenas letras):",text_color="#000000",anchor="w",width=300)
-label_nome.pack(pady=(10, 0))
+label_nome.pack(pady=(2, 0))
 
 entrada_nome = ctk.CTkEntry(frame_cadastro,width=300,validate="key",validatecommand=(janela.register(validar_letras_espacos), "%P"))
-entrada_nome.pack(pady=5)
+entrada_nome.pack(pady=2)
 
-# 2. Campo Quantidade de Livros fisicos
-label_livrosfisicos = ctk.CTkLabel(frame_cadastro,text="Quantidade de livros fisicos lidos (apenas números):",text_color="#000000",anchor="w",width=300)
-label_livrosfisicos.pack(pady=(10, 0))
-entrada_livrosfisicos = ctk.CTkEntry(frame_cadastro,width=300,validate="key",validatecommand=(janela.register(validar_numeros), "%P"))
-entrada_livrosfisicos.pack(pady=5)
+label_idade = ctk.CTkLabel(frame_cadastro, text="Digite sua idade:",text_color="#000000",anchor="w",width=300)
+label_idade.pack(pady=(2, 0))
 
-# 3. Campo Quantidade de Livros digitais
-label_livrosdigitais= ctk.CTkLabel(frame_cadastro,text="Quantidade de livros digitais lidos (apenas números):",text_color="#000000",anchor="w",width=300)
-label_livrosdigitais.pack(pady=(10, 0))
-entrada_livrosdigitais = ctk.CTkEntry(frame_cadastro,width=300,validate="key",validatecommand=(janela.register(validar_numeros), "%P"))
-entrada_livrosdigitais .pack(pady=5)
+entrada_idade = ctk.CTkEntry(frame_cadastro,width=300,validate="key",validatecommand=(janela.register(validar_numeros), "%P"))
+entrada_idade.pack(pady=2)
 
 
-# 4. Campo Horas de Estudo
-label_horas = ctk.CTkLabel(frame_cadastro,text="Horas semanais de leitura para estudo:",text_color="#000000",anchor="w",width=300)
-label_horas.pack(pady=(10, 0))
+#2-Entrada Email
+label_email = ctk.CTkLabel(frame_cadastro, text="Digite seu melhor email:",text_color="#000000",anchor="w",width=300)
+label_email.pack(pady=(2, 0))
+entrada_email = ctk.CTkEntry(frame_cadastro,width=300)
+entrada_email.pack(pady=2)
 
-entrada_horas = ctk.CTkEntry(frame_cadastro,width=300,validate="key",validatecommand=(janela.register(validar_numeros), "%P"))
-entrada_horas.pack(pady=5)
-
-# 5. Campo Horas de Estudo
-label_entretenimento = ctk.CTkLabel(frame_cadastro,text="Horas semanais de leitura por entretenimento(apenas números):",text_color="#000000",anchor="w",width=300)
-label_entretenimento.pack(pady=(10, 0))
-
-entrada_entretenimento = ctk.CTkEntry(frame_cadastro,width=300,validate="key",validatecommand=(janela.register(validar_numeros), "%P"))
-entrada_entretenimento.pack(pady=5)
-
-
-
-
-# 6. Campo Senha
+# 3-Entrada Senha
 label_senha = ctk.CTkLabel(frame_cadastro,text="Senha (mínimo 4 caracteres):",text_color="#000000",anchor="w",width=300)
-label_senha.pack(pady=(10, 0))
+label_senha.pack(pady=(2, 0))
 
 entrada_senha = ctk.CTkEntry(frame_cadastro,width=300,show="*")
-entrada_senha.pack(pady=5)
+entrada_senha.pack(pady=2)
 
-#7.Combox Estados
+# 4. Campo Quantidade de Livros fisicos
+label_livrosfisicos = ctk.CTkLabel(frame_cadastro,text="Quantidade de livros fisicos lidos (apenas números):",text_color="#000000",anchor="w",width=300)
+label_livrosfisicos.pack(pady=(2, 0))
+entrada_livrosfisicos = ctk.CTkEntry(frame_cadastro,width=300,validate="key",validatecommand=(janela.register(validar_numeros), "%P"))
+entrada_livrosfisicos.pack(pady=2)
+
+# 5. Campo Quantidade de Livros digitais
+label_livrosdigitais= ctk.CTkLabel(frame_cadastro,text="Quantidade de livros digitais lidos (apenas números):",text_color="#000000",anchor="w",width=300)
+label_livrosdigitais.pack(pady=(2, 0))
+entrada_livrosdigitais = ctk.CTkEntry(frame_cadastro,width=300,validate="key",validatecommand=(janela.register(validar_numeros), "%P"))
+entrada_livrosdigitais .pack(pady=2)
+
+
+# 6. Campo Horas de Estudo
+label_horas = ctk.CTkLabel(frame_cadastro,text="Horas semanais de leitura para estudo:",text_color="#000000",anchor="w",width=300)
+label_horas.pack(pady=(2, 0))
+
+entrada_horas = ctk.CTkEntry(frame_cadastro,width=300,validate="key",validatecommand=(janela.register(validar_numeros), "%P"))
+entrada_horas.pack(pady=2)
+
+# 7. Campo Horas de Estudo
+label_entretenimento = ctk.CTkLabel(frame_cadastro,text="Horas semanais de leitura por entretenimento(apenas números):",text_color="#000000",anchor="w",width=300)
+label_entretenimento.pack(pady=(2, 0))
+
+entrada_entretenimento = ctk.CTkEntry(frame_cadastro,width=300,validate="key",validatecommand=(janela.register(validar_numeros), "%P"))
+entrada_entretenimento.pack(pady=2)
+
+#8-Entrada preferência de leitura
+combobox_preferencia = ttk.Combobox(frame_cadastro, values=["Digital(kindle)","Livro físico"], state="readonly", width=35)
+#define o texto padrão do combobox
+combobox_preferencia.set("Selecione sua preferência de leitura")
+combobox_preferencia.pack(pady=2)
+#9.Combox Estados
+
 # Combobox de estados
-combobox_estado = ctk.CTkComboBox(frame_cadastro, values=list(estados_siglas.keys()))
-combobox_estado.pack(pady=10)
+# Combobox de Estado (readonly)=impossibilita o usuário de editar a combobox,tendo apenas aquelas opções 
+combobox_estado = ttk.Combobox(frame_cadastro, values=list(estados_siglas.keys()), state="readonly", width=35)
+#define o texto padrão do combobox
 combobox_estado.set("Selecione o estado")
+combobox_estado.pack(pady=2)
+#Quando o usuário escolhe algum valor(comboxselected),a função atualizar cidades é chamada pra fazer a ligação com a API e assim poder pegar os valores das cidades
 combobox_estado.bind("<<ComboboxSelected>>", atualizar_cidades)
 
-# Combobox de cidades (inicialmente vazia)
-combobox_cidade = ctk.CTkComboBox(frame_cadastro, values=["Primeiro selecione o estado"])
+#10- Combobox de Cidade (readonly, inicia vazio)
+combobox_cidade = ttk.Combobox(frame_cadastro, values=[""], state="readonly", width=35)
+combobox_cidade.set("Primeiro selecione o estado")
 combobox_cidade.pack(pady=10)
-combobox_cidade.set("Cidade") 
 
 # Botão de cadastro
-botao_cadastrar = ctk.CTkButton(frame_cadastro, text="Cadastrar",fg_color="blue",width=300,command=cadastrar_conta)
-botao_cadastrar.pack(pady=20)
+botao_cadastrar = ctk.CTkButton(frame_cadastro, text="Cadastrar",fg_color="blue",text_color="#ffffff",width=300,command=cadastrar_conta())
+botao_cadastrar.pack(pady=2)
+botao_voltarinicial=ctk.CTkButton(frame_cadastro, text="Voltar",fg_color="blue",text_color="#ffffff",width=300,command=voltar_inicial)
+botao_voltarinicial.pack()
 
 
 
@@ -250,12 +318,10 @@ class Cadastro:
             #return para a função que estava sendo rodada e deixa rodando apenas a função que rodará
                 return
             else:
-                print("Número de caracteres inválido. Sua senha deve ter entre 4 e 20 caracteres.")
-                self.senha = input("Digite sua senha novamente: ").strip()
-                tentativas -= 1
-                print(f"Tentativas restantes: {tentativas}")
+                label_aviso.config(text="Tamanho da senha inválido",text_color="red")
+                return
 
-        print("Número máximo de tentativas atingido. Tente novamente mais tarde.")
+        
 
     def email_valido(self):
         #FUNÇÃO UTILIZADA PARA CONFERIR SE O EMAIL É VÁLIDO OU NÃO
@@ -268,20 +334,16 @@ class Cadastro:
         while tentativas_email != 0:
             # VERIFICA SE O FORMATO DO EMAIL ESTÁ ESCRITO CORRETAMENTE
             if not re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', self.email):
-                print("FORMATO DE EMAIL INVÁLIDO, UTILIZE UM DOMÍNIO VÁLIDO")
-                self.email = input("Digite novamente seu email: ").strip()
-                tentativas_email -= 1
-                print(f"Tentativas restantes: {tentativas_email}")
+                label_aviso.config(text="FORMATO DE EMAIL INVÁLIDO",text_color="red")
+                return
 
                 continue  # volta pro início do while para validar de novo,caso esteja correto,irá passar pelo verificador
 
             # VERIFICA APENAS O DOMÍNIO,SEPARA TODO O RESTO E PEGA APENAS A PARTE DO DOMÍNIO
             dominio = self.email.split('@')[1].lower()
             if dominio not in dominios_validos:
-                print("Domínio não aceito. Use: Gmail, Outlook, Yahoo, iCloud, etc.")
-                self.email = input("Digite novamente seu email: ").strip()
-                tentativas_email -= 1
-                print(f"Tentativas restantes: {tentativas_email}")
+                label_aviso.config(text="UTILIZE UM DOMÍNIO VÁLIDO PARA O EMAIL",text_color="red")
+                return
 
                 # continuar o loop sem parar
                 continue
@@ -299,7 +361,7 @@ class Cadastro:
     def conferir_email(self):
         #FUNÇÃO UTILIZADA PARA CONFERIR SE O EMAIL JÁ ESTÁ CADASTRADO OU NÃO
         
-        with open(r"banco_dados.JSON", "r", encoding="utf-8") as arquivo:
+        with open(r"dados_usuarios.json", "r", encoding="utf-8") as arquivo:
             arquivo_lido = json.load(arquivo)
             dados_conta = arquivo_lido["senha"]
             dados_familia = arquivo_lido["familia"]
